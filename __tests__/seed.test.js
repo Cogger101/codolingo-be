3996f258720f6edcce67b4e2c81cb478094b918d
thompsurn <121163412+thompsurn@users.noreply.github.com>
2024-05-21 10:54:43 +0100

    completed seed file and tests

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -15,11 +15,51 @@ describe("seed()", () => {
 				.find()
 				.toArray()
 				.then((result) => {
-			    console.log(result);
+				  expect(result.length).toBe(2)
 			    result.forEach(lesson => {
 			      expect(lesson).toHaveProperty("_id", expect.any(Number));
 			      expect(lesson).toHaveProperty("questions", expect.any(Array))
-			      expect(lesson.questions).toHaveLength(4)
+			    });
+				});
+		});
+	});
+
+	describe("users collection", () => {
+		test("Users collection data is inserted correctly", () => {
+			return db
+				.collection("users")
+				.find()
+				.toArray()
+				.then((result) => {
+				  expect(result.length).toBe(3)
+			    result.forEach(user => {
+			      expect(user).toHaveProperty("_id", expect.any(Object));
+			      expect(user).toHaveProperty("user_name", expect.any(String))
+				  expect(user).toHaveProperty("score", expect.any(Number));
+			      expect(user).toHaveProperty("password", expect.any(String))
+				  expect(user).toHaveProperty("avatar_url", expect.any(String));
+			      expect(user).toHaveProperty("friends", expect.any(Array))
+				  expect(user).toHaveProperty("progress", expect.any(Array))
+			    });
+				});
+		});
+	});
+
+	describe("questions collection", () => {
+		test("Questions collection data is inserted correctly", () => {
+			return db
+				.collection("questions")
+				.find()
+				.toArray()
+				.then((result) => {
+				  expect(result.length).toBe(8)
+			    result.forEach(question => {
+			      expect(question).toHaveProperty("_id", expect.any(Number));
+			      expect(question).toHaveProperty("type", expect.any(String))
+				  expect(question).toHaveProperty("teaching", expect.any(String));
+			      expect(question).toHaveProperty("question", expect.any(String))
+			      expect(question).toHaveProperty("answer")
+				  expect(question).toHaveProperty("help_url", expect.any(String))
 			    });
 				});
 		});
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -5,6 +5,12 @@ const db = client.db()
 const seed = async ({ lessonData, userData, questionData }) => {
   await db.collection("lessons").drop()
   await db.collection("lessons").insertMany(lessonData)
+  await db.collection("users").drop()
+  await db.collection("users").createIndex({"user_name": 1}, {unique: true})
+  await db.collection("users").insertMany(userData)
+  await db.collection("questions").drop()
+  await db.collection("questions").insertMany(questionData)
 }
+
 
 module.exports = seed
