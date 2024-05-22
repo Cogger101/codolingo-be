ea783ac00812ae0d9032b1d7312cc88d870406a1
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-05-22 11:42:59 +0100

    api endpoint

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,13 +1,16 @@
 const apiRouter = require("express").Router();
 
-const lessonsRouter = require('./lessons-router')
-const questionsRouter = require('./questions-router')
-const usersRouter = require('./users-router')
+const { getEndpoints } = require("../controllers/api-controller");
+const lessonsRouter = require("./lessons-router");
+const questionsRouter = require("./questions-router");
+const usersRouter = require("./users-router");
 
-apiRouter.use("/lessons", lessonsRouter)
+apiRouter.get("/", getEndpoints);
 
-apiRouter.use("/users", usersRouter)
+apiRouter.use("/lessons", lessonsRouter);
 
-apiRouter.use("/questions", questionsRouter)
+apiRouter.use("/users", usersRouter);
+
+apiRouter.use("/questions", questionsRouter);
 
 module.exports = apiRouter;
