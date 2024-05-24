cbf4d6ea8360b7b22f1e8df9e7dfd20b5cfe0655
Cogger101 <seandcoughlan@hotmail.co.uk>
2024-05-24 14:49:46 +0100

    Amended the db lessons

--- db/data/development-data/lessons.js
+++ db/data/development-data/lessons.js
@@ -1,10 +1,18 @@
 module.exports = [
   {
     _id: 1,
-    questions: [1, 2, 3, 4]
+    questions: [1, 2, 3, 4, 5]
   },
   {
     _id: 2,
-    questions: [5, 6, 7, 8]
+    questions: [6, 7, 8, 9, 10]
+  },
+  {
+    _id: 3,
+    questions: [11, 12, 13, 14, 15]
+  },
+  {
+    _id: 4,
+    questions: [16, 17, 18, 19, 20]
   }
 ]
