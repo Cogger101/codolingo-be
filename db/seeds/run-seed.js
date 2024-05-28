24467b11e8eb1a39c7a4833c3c1e5002ac5fd9c1
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-05-28 16:25:53 +0100

    added a readme

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,5 +1,5 @@
-const devData = require('../data/development-data/index.js');
-const seed = require('./seed.js');
+const devData = require("../data/development-data/index.js");
+const seed = require("./seed.js");
 const client = require(`${__dirname}/../connection.js`);
 
 const runSeed = () => {
